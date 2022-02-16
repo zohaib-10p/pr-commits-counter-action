e2560dd2f896662e43871e3d5b6d27ed62e291d1
Zohaib S <zohaib.shahjahan@10pearls.com>
2022-02-16 19:19:57 +0500

    More Input added

--- dist/index.js
+++ dist/index.js
@@ -8471,13 +8471,30 @@ var __webpack_exports__ = {};
 const gitCore = __nccwpck_require__(2186);
 const gitHub = __nccwpck_require__(5438);
 
-const run = async() => {
- const owner = gitCore.getInput('owner');
- const prNumber = gitCore.getInput('pr-number');
- gitCore.setOutput(`The owner of the repository is ${owner} PR is ${prNumber}`);
+const run = async () => {
+   const owner = gitCore.getInput('owner');
+   const prNumber = gitCore.getInput('pr-number');
+   const repository = gitCore.getInput('repository');
+   const token = gitCore.getInput('github-token');
+   const maxAllowedCommits = gitCore.getInput('max-allowed-commits');
+   const client = gitHub.getOctokit(token);
+   const commits = await client.rest.pulls.listCommits({
+      owner: owner,
+      repo: repository,
+      pull_number: prNumber,
+   });
+   gitCore.setCommandEcho(true);
+   if(commits.data.length > maxAllowedCommits){
+      gitCore.setFailed(`Your PR has ${commits.data.length} commits. Maximum allowed commits in a PR is ${maxAllowedCommits}`);
+   } else {
+      gitCore.setOutput('totalCommits', commits.data.length);
+   }
+   
 };
 
 run();
+
+
 })();
 
 module.exports = __webpack_exports__;
--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const gitCore = require('@actions/core');
 const gitHub = require('@actions/github');
 
-const run = async() => {
- const owner = gitCore.getInput('owner');
- const prNumber = gitCore.getInput('pr-number');
- gitCore.setOutput(`The owner of the repository is ${owner} PR is ${prNumber}`);
+const run = async () => {
+   const owner = gitCore.getInput('owner');
+   const prNumber = gitCore.getInput('pr-number');
+   const repository = gitCore.getInput('repository');
+   const token = gitCore.getInput('github-token');
+   const maxAllowedCommits = gitCore.getInput('max-allowed-commits');
+   const client = gitHub.getOctokit(token);
+   const commits = await client.rest.pulls.listCommits({
+      owner: owner,
+      repo: repository,
+      pull_number: prNumber,
+   });
+   gitCore.setCommandEcho(true);
+   if(commits.data.length > maxAllowedCommits){
+      gitCore.setFailed(`Your PR has ${commits.data.length} commits. Maximum allowed commits in a PR is ${maxAllowedCommits}`);
+   } else {
+      gitCore.setOutput('totalCommits', commits.data.length);
+   }
+   
 };
 
 run();
+
